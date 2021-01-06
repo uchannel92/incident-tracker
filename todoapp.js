abc2fb6950a52fb4b51b8380dfa914f70d88c3b5
Uchannel92 <uchenna_attah@homtail.com>
2021-01-06 16:16:51 +0000

    Completed design for homepage and raise incident form.

--- todoapp.js
+++ todoapp.js
@@ -1,139 +1,34 @@
-// ToDo empty array
-
-// const todo = [];
-// let input = prompt("Type something..")
-
-// while (input === "q") {
-//     break;
-
-//     if (input === "add") {
-        
-//     }
-// }
-
-// console.log("App exited..")
-
-// below commands to implement:
-// new: (Not implemented)
-// list: (Not implemented)
-// delete: (Not implemented)
-// quit: (Not implemented) `
-
-// while loop
-// let input = prompt(`Todo App Options: new, delete, list, quit ${todo}`)
-
-// while(input !== "quit") {
-
-//     if (todo === "new") { 
-//         prompt(`Add to list: ${todo.push()}`)
-//         console.log(todo)      
-//         console.log("Add a new todo");
-//     } 
-
-    // if (prompt === "delete") {
-    //     console.log("delete from todo");
-    // }
-
-    // if (prompt === "list") {
-    //     console.log("list the todo");
-    // }
-
-    // if (prompt() === "quit") {
-    //     console.log("Exit the app..");
-    // }
-// }
-
-// quit function
-
-// ###############
-
-// input = array.push(prompt(`Add new players to array:`));
-// input = array.push(prompt(`Add new players to array:`));
-
-// const array = [1]
-
-// asking to prompt 3 times..
-
-// while(array !== 'quit') {
-//     array.push(prompt("Add another name"));
-
-//     if (array === "quit") {
-//         break;
-//     }
- 
-//  }
-
-// console.log(array)
-
-// Iterating Each item in array...
-// for (let i = 0; i < array.length; i++) {
-//     console.log(i, array[i])
-// }
-
-// Accomplished - adding to array and listing array
-
-// outstanding
-// My issue is using the while loop and breaking while loop. Syntax
-// implement the quit
-// delete
-// quit
-// list iterarted array on command.
-
-// how to loop and exit
-// const exit = "exit";
-
-// let todo = prompt("Todo options. type commands to continue: add|view|delete|exit.");
-
-// let array = []
-
-// while(array !== exit) {
-//     todo = prompt(`Repeat: Todo options, press 'exit' to exit.`);
-
-// }
-// console.log("You have quit, type anything to exit.")
-
-
-// how break command works.
-for(let i = 10; i > 0; i --) {
-    console.log(i)
-    if (i === 2) break;
-}
-
-// quit function has been implemented...!!
-
 const array = []
 
-let input = prompt("Todo hit enter to continue..")
+let input = prompt("Todo: Hit the enter key to continue..")
 
-while(input !== "q") {
+while(input !== "q" && input !== "quit") {
 
-        input = prompt("Type the following to continue - quit | view | new | delete | deleteM | length | first | last ")
+        input = prompt(`Type the following to continue - 
+        quit | view | new | delete | length | first | last `)
 
     if (input === "view") {
+        
+        console.log("*****")
         for (let i = 0; i < array.length; i ++) {
             console.log(i, array[i])         
+        }
+        console.log("*****")
+
+        if (array.length < 1) {
+            console.log("There are no items to display..")
         }
     }
 
     if (input === "new") {
         input = array.push(prompt("Add a new item to array"));
-        console.log(`${array[array.length -1]} has been added.`)
+        console.log(`"${array[array.length -1]}" has been added.`)
     }
 
+    // To use splice, the user will prompt the index in the input, the 1 at the end indicates to remove 1 item!
     if (input === "delete") {
-        input = array.pop(prompt("You will be deleting the last item entered, type delete again."))
-        console.log(`${array[array.length -1]} has been deleted.`)
-    }
- 
-    // work on using splice to delete items !!!
-    if (input === "deleteM") {
-        for (let i = 0; i < array.length; i ++) {
-            console.log(i, array[i])         
-        }
-        input = array.splice(prompt("Specify the array index you want to delete.."))
-        for (let i = 0; i < array.length; i ++) {
-            console.log(i, array[i])         
-        }
+        input = array.splice(prompt("Specify the array index you want to delete.."), 1)
+        console.log(`"${input}" has been deleted..`)
     }
 
     // todo length
@@ -154,7 +49,7 @@ while(input !== "q") {
 
     if (input === "last") {
         input = array[array.length -1]
-        console.log(`${array[array.length -1]} is the last item in the array.`)
+        console.log(`"${array[array.length -1]}" is the last item in the array.`)
     }
 
 }
